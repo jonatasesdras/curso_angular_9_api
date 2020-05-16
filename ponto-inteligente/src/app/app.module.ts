83a928ea0122c0be79901d7f937b457de57dc3e0
Jonatas Esdras <filatasjon@gmail.com>
2020-05-16 19:11:19 -0300

    Pacotes instalados

--- ponto-inteligente/src/app/app.module.ts
+++ ponto-inteligente/src/app/app.module.ts
@@ -1,5 +1,9 @@
+import { browser } from 'protractor';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { browserAnimationsModule } from '@angular/plataform-browser/animations';
+import { MatButtonModule } from '@angular/material'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
